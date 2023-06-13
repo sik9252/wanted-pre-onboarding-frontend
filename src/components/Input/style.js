80f43576cd2a5118d1428c41b75097cde2e3d7df
sik9252 <sik9252@naver.com>
2023-06-13 09:36:46 +0900

    feat: Input 컴포넌트 생성

--- src/components/Input/style.js
+++ src/components/Input/style.js
@@ -1,1 +1,19 @@
 import styled from "styled-components";
+import COLOR from "../../styles/colors";
+
+export const StyledInputBox = styled.input`
+  width: 100%;
+  height: ${({ height }) => (height ? `${height}px` : "58px")};
+  border: none;
+  padding: 10px;
+  border-radius: 7px;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+
+  ::placeholder {
+    color: ${COLOR.GREEN_9};
+  }
+
+  :focus {
+    outline: none;
+  }
+`;
