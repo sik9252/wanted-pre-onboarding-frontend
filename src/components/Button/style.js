24ce1253aee32c2bb62cd0a017d969a671b90396
sik9252 <sik9252@naver.com>
2023-06-13 09:31:19 +0900

    feat: Button 컴포넌트 생성

--- src/components/Button/style.js
+++ src/components/Button/style.js
@@ -1,1 +1,16 @@
 import styled from "styled-components";
+import COLOR from "../../styles/colors";
+
+export const StyledButton = styled.button`
+  width: ${({ width }) => `${width}px`};
+  height: ${({ height }) => `${height}px`};
+  background-color: ${COLOR.GREEN_7};
+  color: ${COLOR.WHITE};
+  border-radius: 7px;
+
+  :hover {
+    cursor: pointer;
+    transition: 0.3s;
+    background-color: ${COLOR.GREEN_6};
+  }
+`;
