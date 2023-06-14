430601761ed1abfccfc446351eebd803b3f497fe
sik9252 <sik9252@naver.com>
2023-06-14 16:10:39 +0900

    feat: 로그인 axios 요청 추가

--- src/utils/axiosManage/AuthAxios/index.js
+++ src/utils/axiosManage/AuthAxios/index.js
@@ -4,3 +4,8 @@ import { useAxios } from "../useAxios";
 export const signUpRequest = async (inputData) => {
   return await useAxios.post(`/auth/signup`, inputData);
 };
+
+/** 로그인 요청 */
+export const signInRequest = async (inputData) => {
+  return await useAxios.post(`/auth/signin`, inputData);
+};
