3c2651459a07556dd1a7f05aa326fcb2b22fc9ec
sik9252 <sik9252@naver.com>
2023-06-14 16:10:27 +0900

    feat: axios 요청 헤더에 content-type과 authorization 추가

--- src/utils/axiosManage/useAxios.js
+++ src/utils/axiosManage/useAxios.js
@@ -2,4 +2,8 @@ import axios from "axios";
 
 export const useAxios = axios.create({
   baseURL: "https://www.pre-onboarding-selection-task.shop/",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+  },
 });
