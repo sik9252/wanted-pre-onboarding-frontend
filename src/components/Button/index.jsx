af33476594b50dc9de90e78b70c201ce7fdbf7d7
sik9252 <sik9252@naver.com>
2023-06-14 15:58:17 +0900

    feat: 회원가입 기능 구현 완료

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { StyledButton } from "./style";
 
-export const Button = ({ children, width, height, onClick, isDisabled }) => {
+export const Button = ({
+  children,
+  width,
+  height,
+  onClick,
+  isAllValidateChecked,
+}) => {
   return (
     <StyledButton
       width={width}
       height={height}
       onClick={onClick}
-      disabled={isDisabled ? "disabled" : ""}
+      disabled={isAllValidateChecked ? "" : "disabled"}
     >
       {children}
     </StyledButton>
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,17 +1,84 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 /** components */
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
-import { ErrorBox } from "../../components/ErrorBox";
 
 /** styles */
 import { SignUpContainer, Title, InputBox, ButtonBox } from "./style";
 
+/** axios */
+import { signUpRequest } from "../../utils/axiosManage/AuthAxios";
+
+/** other utils */
+import {
+  checkEmailValidate,
+  checkPasswordValidate,
+} from "../../utils/InputManage/checkInputValidate";
+
 function SignUp() {
-  // 회원가입 폼 제출
+  const navigate = useNavigate();
+
+  // 입력한 데이터 유효성 검증 여부
+  const [isValidatedEmail, setIsValidatedEmail] = useState(false);
+  const [isValidatedPassword, setIsValidatedPassword] = useState(false);
+  const [isAllValidateChecked, setIsAllValidateChecked] = useState(false);
+
+  // 유저가 입력한 이메일
+  const [userEmail, setUserEmail] = useState("");
+  const onChangeUserEmail = (e) => {
+    setUserEmail(e.target.value);
+  };
+
+  // 유저가 입력한 비밀번호
+  const [userPassword, setUserPassword] = useState("");
+  const onChangeUserPassword = (e) => {
+    setUserPassword(e.target.value);
+  };
+
+  // 유저가 이메일, 비밀번호를 입력할때마다 유효성 검증
+  useEffect(() => {
+    if (checkEmailValidate(userEmail)) {
+      setIsValidatedEmail(true);
+    } else {
+      setIsValidatedEmail(false);
+    }
+
+    if (checkPasswordValidate(userPassword)) {
+      setIsValidatedPassword(true);
+    } else {
+      setIsValidatedPassword(false);
+    }
+  }, [userEmail, userPassword]);
+
+  useEffect(() => {
+    if (isValidatedEmail && isValidatedPassword) {
+      setIsAllValidateChecked(true);
+    } else {
+      setIsAllValidateChecked(false);
+    }
+  }, [isValidatedEmail, isValidatedPassword]);
+
+  // 회원가입 폼 제출 함수
   const clickSignUpButton = () => {
-    console.log("rer");
+    if (isAllValidateChecked) {
+      const inputData = {
+        email: userEmail,
+        password: userPassword,
+      };
+
+      signUpRequest(inputData)
+        .then((res) => {
+          if (res) {
+            alert("회원가입이 완료되었습니다!");
+            navigate("/signin");
+          }
+        })
+        .catch((error) => {
+          alert(error.response.data.message);
+        });
+    }
   };
 
   return (
@@ -19,16 +86,19 @@ function SignUp() {
       <Title>회원가입</Title>
       <InputBox>
         <div>
-          <Input data-testid="email-input" placeholder={"이메일"} />
-          <ErrorBox>올바르지 않은 이메일 형식입니다.</ErrorBox>
+          <Input
+            data-testid="email-input"
+            placeholder={"이메일"}
+            onChange={onChangeUserEmail}
+          />
         </div>
         <div>
           <Input
             data-testid="password-input"
             type={"password"}
-            placeholder={"비밀번호"}
+            placeholder={"비밀번호 (8자리 이상)"}
+            onChange={onChangeUserPassword}
           />
-          <ErrorBox>비밀번호는 8자리 이상입니다.</ErrorBox>
         </div>
       </InputBox>
       <ButtonBox>
@@ -36,8 +106,8 @@ function SignUp() {
           data-testid="signup-button"
           width={200}
           height={40}
-          isDisabled={true}
-          onClick={clickSignUpButton}
+          isAllValidateChecked={isAllValidateChecked}
+          onClick={() => clickSignUpButton()}
         >
           회원가입
         </Button>
