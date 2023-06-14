5ebedef03213d8ab6ab82ad0727f6fe3f4c28f61
sik9252 <sik9252@naver.com>
2023-06-14 14:27:02 +0900

    feat: 회원가입 페이지 UI 구현 완료

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,7 +1,49 @@
 import React from "react";
 
+/** components */
+import { Button } from "../../components/Button";
+import { Input } from "../../components/Input";
+import { ErrorBox } from "../../components/ErrorBox";
+
+/** styles */
+import { SignUpContainer, Title, InputBox, ButtonBox } from "./style";
+
 function SignUp() {
-  return <div>SignUp</div>;
+  // 회원가입 폼 제출
+  const clickSignUpButton = () => {
+    console.log("rer");
+  };
+
+  return (
+    <SignUpContainer>
+      <Title>회원가입</Title>
+      <InputBox>
+        <div>
+          <Input data-testid="email-input" placeholder={"이메일"} />
+          <ErrorBox>올바르지 않은 이메일 형식입니다.</ErrorBox>
+        </div>
+        <div>
+          <Input
+            data-testid="password-input"
+            type={"password"}
+            placeholder={"비밀번호"}
+          />
+          <ErrorBox>비밀번호는 8자리 이상입니다.</ErrorBox>
+        </div>
+      </InputBox>
+      <ButtonBox>
+        <Button
+          data-testid="signup-button"
+          width={200}
+          height={40}
+          isDisabled={true}
+          onClick={clickSignUpButton}
+        >
+          회원가입
+        </Button>
+      </ButtonBox>
+    </SignUpContainer>
+  );
 }
 
 export default SignUp;
--- src/pages/SignUp/style.js
+++ src/pages/SignUp/style.js
@@ -1,1 +1,27 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+export const SignUpContainer = styled.div`
+  width: 500px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+export const Title = styled.div`
+  font-size: 25px;
+  font-weight: 600;
+  text-align: center;
+  margin-bottom: 20px;
+`;
+
+export const InputBox = styled.div`
+  & > div {
+    margin: 5px 0;
+  }
+`;
+
+export const ButtonBox = styled.div`
+  text-align: center;
+  margin-top: 20px;
+`;
